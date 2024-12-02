70e6d4065ec1a6a090fe38226aa5fe55024de19f
Ashpool <ashpool@xecut.net>
2024-12-02 16:54:57 +0100

    solve: 2a, 2b

---
+++ day2/b.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+(require
+ (prefix-in type: relation/type)
+ (prefix-in seq: seq))
+
+(define (string->numbers str)
+  (map string->number (string-split str)))
+
+(define (levels-safe? levels trend-before)
+  (match levels
+    [(list) #t]
+    [(list _) #t]
+    [(cons head tail)
+     (let*
+         ([next (first tail)]
+          [step (- next head)]
+          [trend (cond [(positive? step) 'trend-up]
+                       [(negative? step) 'trend-down]
+                       [else 'trend-constant])])
+       (and (not (eq? trend 'trend-constant))
+            (or (eq? trend-before 'trend-unknown)
+                (eq? trend-before trend))
+            (<= (abs step) 3)
+            (levels-safe? tail trend)))]))
+
+(define report%
+  (class object%
+    (init-field (levels (list)))
+    (define/public (safe?)
+      (levels-safe? (get-field levels this) 'trend-unknown))
+    (define/public (dampened-safe?)
+      (or (send this safe?)
+          (let ([levels (get-field levels this)])
+            (for/or ([index (in-range (length levels))])
+              (levels-safe? (type:->list (seq:remove-at index levels))
+                            'trend-unknown)))))
+    (super-new)))
+
+(define (string->report str)
+  (new report% [levels (string->numbers str)]))
+
+(define (main)
+  (seq:length
+   (seq:filter
+    identity
+    (seq:map
+     (compose (λ (r) (send r dampened-safe?)) string->report)
+     (type:->stream (in-lines))))))
+
+(with-input-from-file "input" main)
