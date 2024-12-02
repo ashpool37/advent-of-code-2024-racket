b8861bd692eb374bbdc6be38793b2e1356f579fc
Ashpool <ashpool@xecut.net>
2024-12-02 17:02:48 +0100

    feat: 2: simplify (eliminate classes)

--- day2/a.rkt
+++ day2/a.rkt
@@ -7,38 +7,29 @@
 (define (string->numbers str)
   (map string->number (string-split str)))
 
-(define report%
-  (class object%
-    (init-field (levels (list)))
-    (define/public (safe?)
-      (define (levels-safe? levels trend-before)
-        (match levels
-          [(list) #t]
-          [(list _) #t]
-          [(cons head tail)
-           (let*
-               ([next (first tail)]
-                [step (- next head)]
-                [trend (cond [(positive? step) 'trend-up]
-                             [(negative? step) 'trend-down]
-                             [else 'trend-constant])])
-             (and (not (eq? trend 'trend-constant))
-                  (or (eq? trend-before 'trend-unknown)
-                      (eq? trend-before trend))
-                  (<= (abs step) 3)
-                  (levels-safe? tail trend)))]))
-      (levels-safe? (get-field levels this) 'trend-unknown))
-    (super-new)))
-
-(define (string->report str)
-  (new report% [levels (string->numbers str)]))
+(define (levels-safe? levels [trend-before 'trend-unknown])
+  (match levels
+    [(list) #t]
+    [(list _) #t]
+    [(cons head tail)
+     (let*
+         ([next (first tail)]
+          [step (- next head)]
+          [trend (cond [(positive? step) 'trend-up]
+                       [(negative? step) 'trend-down]
+                       [else 'trend-constant])])
+       (and (not (eq? trend 'trend-constant))
+            (or (eq? trend-before 'trend-unknown)
+                (eq? trend-before trend))
+            (<= (abs step) 3)
+            (levels-safe? tail trend)))]))
 
 (define (main)
   (seq:length
    (seq:filter
     identity
     (seq:map
-     (compose (λ (r) (send r safe?)) string->report)
+     (compose levels-safe? string->numbers)
      (type:->stream (in-lines))))))
 
 (with-input-from-file "input" main)
