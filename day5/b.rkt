f6dc326c25a608b89ef6e0c79fbb3e2ae259febd
Ashpool <ashpool@xecut.net>
2024-12-05 17:11:46 +0100

    feat: 5a: refactor, optimise

--- day5/b.rkt
+++ day5/b.rkt
@@ -13,48 +13,52 @@
     (if match-result (map bytes->number (cdr match-result))
         #f)))
 
-(define (read-printing-order)
-  (let ([match-result
-         (regexp-try-match #px"\\s*((?:\\d{2},)*\\d{2})"
-                           (current-input-port))])
-    (if match-result
-        (map
-         string->number
-         (string-split (bytes->string/utf-8 (second match-result)) ","))
-        #f)))
+(struct update (page-order dep-graph))
+
+(define (update-correct? the-update)
+  (let ([positions (make-hash)])
+    (for ([(page page-position)
+           (in-indexed (update-page-order the-update))])
+      (hash-set! positions page page-position))
+    (for/and ([rule (in-edges (update-dep-graph the-update))])
+      (let ([first-pos (hash-ref positions (first rule) #f)]
+            [second-pos (hash-ref positions (second rule) #f)])
+        (or (not first-pos)
+            (not second-pos)
+            (< first-pos second-pos))))))
+
+(define (update-tsort the-update)
+ (update (tsort (update-dep-graph the-update))
+         (graph-copy (update-dep-graph the-update))))
+
+(define (update-middle-page the-update)
+ (list-ref (update-page-order the-update)
+           (exact-floor (/ (length (update-page-order the-update))
+                           2))))
 
 (define (main)
   (let*
       ([rules (->list (in-producer read-printing-rule #f))]
-       [orders (->stream (in-producer read-printing-order #f))]
-       [order-correct?
-        (λ (order)
-          (let ([positions (make-hash)])
-            (for ([page (in-list order)]
-                  [page-position (in-range (length order))])
-              (hash-set! positions page page-position))
-            (for/and ([rule rules])
-              (let ([first-pos (hash-ref positions (first rule) #f)]
-                    [second-pos (hash-ref positions (second rule) #f)])
-                (or (not first-pos)
-                    (not second-pos)
-                    (< first-pos second-pos))))))]
-       [order-tsort
-        (λ (order)
-          (let ([dep-graph (unweighted-graph/directed null)])
-            (for ([page order]) (add-vertex! dep-graph page))
-            (for ([rule rules])
-              (when (and (has-vertex? dep-graph (first rule))
-                         (has-vertex? dep-graph (second rule)))
-                (apply add-directed-edge! dep-graph rule)))
-            (tsort dep-graph)))]
-       [order-middle-page
-        (λ (order)
-          (list-ref order (exact-floor (/ (length order) 2))))])
+       [read-update
+        (thunk
+         (let* ([match-result (regexp-try-match #px"\\s*((?:\\d{2},)*\\d{2})"
+                                                (current-input-port))])
+           (if (not match-result) #f
+               (let*
+                   ([capture (second match-result)]
+                    [tokens (string-split (bytes->string/utf-8 capture) ",")]
+                    [page-order (map string->number tokens)]
+                    [dep-graph (unweighted-graph/directed null)])
+                 (for ([page page-order]) (add-vertex! dep-graph page))
+                 (for ([rule rules])
+                   (when (and (has-vertex? dep-graph (first rule))
+                              (has-vertex? dep-graph (second rule)))
+                     (apply add-directed-edge! dep-graph rule)))
+                 (update page-order dep-graph)))))]
+       [updates (->stream (in-producer read-update #f))])
     (seq:foldl
      + 0
-     (seq:map
-      (compose order-middle-page order-tsort)
-      (seq:filter (negate order-correct?) orders)))))
+     (seq:map (compose update-middle-page update-tsort)
+              (seq:filter (negate update-correct?) updates)))))
 
 (with-input-from-file "input" main)
