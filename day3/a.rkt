3b1982c67e0737c32ef8b5a300564dab9cb00dbd
Ashpool <ashpool@xecut.net>
2024-12-06 18:05:26 +0100

    feat: 3: refactor (use at-exps, split into functions)

--- day3/a.rkt
+++ day3/a.rkt
@@ -1,17 +1,18 @@
-#lang racket
+#lang at-exp racket
 
-(require (prefix-in seq: seq))
+(require (prefix-in seq: seq)
+         relation/type)
+
+(define (read-mul)
+  (regexp-try-match @pregexp{mul\((\d{1,3}),(\d{1,3})\)}
+                    (current-input-port)))
 
 (define bytes->number (compose string->number bytes->string/utf-8))
 
+(define (eval-mul match-result)
+  (apply * (map bytes->number (cdr match-result))))
+
 (define (main)
-  (seq:foldl
-   + 0
-   (seq:map (compose (λ (ns) (apply * (map bytes->number ns)))
-                     cdr)
-            (sequence->stream
-             (in-producer (thunk (regexp-match #px"mul\\((\\d{1,3}),(\\d{1,3})\\)"
-                                               (current-input-port)))
-                          #f)))))
+  (seq:foldl + 0 (seq:map eval-mul (->stream (in-producer read-mul #f)))))
 
 (with-input-from-file "input" main)
--- day3/b.rkt
+++ day3/b.rkt
@@ -1,22 +1,23 @@
-#lang racket
+#lang at-exp racket
+
+(define (read-op)
+  (regexp-try-match
+   @pregexp{do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)}
+   (current-input-port)))
 
 (define bytes->number (compose string->number bytes->string/utf-8))
 
 (define (main)
-  (let ([enable-mul? #t]
+  (let ([is-mul-enabled #t]
         [acc 0])
-    (for ([m (in-producer
-              (thunk (regexp-match
-                      #px"do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\)"
-                      (current-input-port)))
-              #f)])
+    (for ([m (in-producer read-op #f)])
       (match m
         [(list #"do()" #f #f)
-         (set! enable-mul? #t)]
+         (set! is-mul-enabled #t)]
         [(list #"don't()" #f #f)
-         (set! enable-mul? #f)]
+         (set! is-mul-enabled #f)]
         [(list _ (app bytes->number a) (app bytes->number b))
-         (when enable-mul? (set! acc (+ acc (* a b))))]))
+         (when is-mul-enabled (set! acc (+ acc (* a b))))]))
     acc))
 
 (with-input-from-file "input" main)
