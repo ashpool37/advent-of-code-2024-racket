65bf1813938b0376a3a3e3d9c795f7ceb645c806
Ashpool <ashpool@xecut.net>
2024-12-08 07:50:42 +0100

    solve: 8a, 8b

---
+++ day8/a.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+(require relation/type
+         (prefix-in seq: seq))
+
+(struct vec2d (x y) #:transparent)
+(define (vec2d+ a b)
+  (vec2d (+ (vec2d-x a) (vec2d-x b))
+         (+ (vec2d-y a) (vec2d-y b))))
+(define (vec2d- a b)
+  (vec2d (- (vec2d-x a) (vec2d-x b))
+         (- (vec2d-y a) (vec2d-y b))))
+
+(define (read-antenna-map [in (current-input-port)])
+  (let ([antenna-locations (make-hash)]
+        [dim-x 0]
+        [dim-y 0])
+    (for ([(line y) (in-indexed (in-lines in))])
+      (set! dim-y (max (add1 y) dim-y))
+      (for ([(freq x) (in-indexed (in-string line))])
+        (set! dim-x (max (add1 x) dim-x))
+        (when (not (char=? freq #\.))
+          (hash-update! antenna-locations
+                        freq
+                        (curry cons (vec2d x y))
+                        null))))
+    (values antenna-locations dim-x dim-y)))
+
+(define (antinodes a1 a2)
+  (let ([delta (vec2d- a2 a1)])
+    (list (vec2d+ a2 delta) (vec2d- a1 delta))))
+
+(define (main)
+  (let*-values
+      ([(antenna-locations dim-x dim-y) (read-antenna-map)]
+       [(in-bounds?)
+        (λ (v) (match v [(vec2d x y) (and (<= 0 x) (<= 0 y)
+                                          (< x dim-x) (< y dim-y))]))]
+       [(antenna-pairs)
+        (seq:append-map (λ (antennas) (->stream (in-combinations antennas 2)))
+                        (->stream (in-hash-values antenna-locations)))]
+       [(antinodes)
+        (seq:deduplicate
+         (seq:filter
+          in-bounds?
+          (seq:append-map
+           (λ (antenna-pair) (apply antinodes antenna-pair))
+           antenna-pairs)))])
+    (seq:length antinodes)))
+
+(with-input-from-file "input" main)
