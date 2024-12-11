5eb68f60b5fd5d5faa3e4b7da72729085f6ff994
Ashpool <ashpool@xecut.net>
2024-12-11 16:56:58 +0100

    solve: 11b

--- src/day11/b.rkt
+++ src/day11/b.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-(require (prefix-in col: data/collection)
+(require (prefix-in seq: seq)
          memo)
 
-(define/memoize (even-length-number-halves num)
+(define (even-length-number-halves num)
   (define number-str (number->string num))
   (define number-strlen (string-length number-str))
   (define number-strlen-half (/ number-strlen 2))
@@ -12,19 +12,22 @@
             (string->number (substring number-str number-strlen-half)))
       #f))
 
-(define/memoize (blink-stone stone)
+(define/memoize (blink-once stone)
   (match stone
-    [0 (list 1)]
-    [(app even-length-number-halves (? list? halves)) halves]
-    [_ (list (* stone 2024))]))
+    [0 (stream 1)]
+    [(app even-length-number-halves (? stream? halves)) halves]
+    [_ (stream (* stone 2024))]))
 
-(define (blink stones)
-  (col:flatten (stream-map blink-stone stones)))
-
-(define (blink-n-times n)
-  (apply compose (make-list n blink)))
+(define/memoize (blink n stone)
+  (if (= 0 n)
+      1
+      (match stone
+        [0 (blink (sub1 n) 1)]
+        [(app even-length-number-halves (list left-half right-half))
+         (+ (blink (sub1 n) left-half) (blink (sub1 n) right-half))]
+        [_ (blink (sub1 n) (* 2024 stone))])))
 
 (define (main)
-  (stream-length ((blink-n-times 75) (sequence->stream (in-port read)))))
+  (seq:foldl + 0 (seq:map (curry blink 75) (sequence->stream (in-port)))))
 
 (with-input-from-file "input" main)
